42294499ede4c1357d25dd245dcff408c9467568
Barros9 <giu.barreca@gmail.com>
2023-07-03 19:21:10 +0200

    Clean work

--- app/src/main/java/com/barros/beerapp/BeerAppApplication.kt
+++ app/src/main/java/com/barros/beerapp/BeerAppApplication.kt
@@ -1,19 +1,7 @@
 package com.barros.beerapp
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-internal class BeerAppApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun getWorkManagerConfiguration() =
-        Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
-}
+internal class BeerAppApplication : Application()
