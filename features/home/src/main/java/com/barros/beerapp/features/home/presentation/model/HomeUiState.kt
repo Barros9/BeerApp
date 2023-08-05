ea7031759a099ad8c640e985c0ea70331df4bee5
Barros9 <giu.barreca@gmail.com>
2023-08-05 15:27:26 +0200

    Analysis

--- features/home/src/main/java/com/barros/beerapp/features/home/presentation/model/HomeUiState.kt
+++ features/home/src/main/java/com/barros/beerapp/features/home/presentation/model/HomeUiState.kt
@@ -3,8 +3,8 @@ package com.barros.beerapp.features.home.presentation.model
 import com.barros.beerapp.libraries.beer.domain.entity.Beer
 
 internal sealed class HomeUiState {
-    object Empty : HomeUiState()
-    object Error : HomeUiState()
-    object Loading : HomeUiState()
+    data object Empty : HomeUiState()
+    data object Error : HomeUiState()
+    data object Loading : HomeUiState()
     data class Success(val beers: List<Beer>) : HomeUiState()
 }
--- libraries/navigator/src/main/java/com/barros/beerapp/libraries/navigator/navigation/NavigatorEvent.kt
+++ libraries/navigator/src/main/java/com/barros/beerapp/libraries/navigator/navigation/NavigatorEvent.kt
@@ -3,8 +3,8 @@ package com.barros.beerapp.libraries.navigator.navigation
 import androidx.navigation.NavOptionsBuilder
 
 sealed class NavigatorEvent {
-    object NavigateUp : NavigatorEvent()
-    object PopBackStack : NavigatorEvent()
+    data object NavigateUp : NavigatorEvent()
+    data object PopBackStack : NavigatorEvent()
     class Directions(val destination: String, val builder: NavOptionsBuilder.() -> Unit) :
         NavigatorEvent()
 }
